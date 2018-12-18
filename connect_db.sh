608447ef37624bdfa2f9b8678c3742a1bc0c9910
JohnFranco Saraceno <jfsaraceno@gmail.com>
2018-12-17 18:14:55 -0800

    reorganized project layout per best prac

--- connect_db.sh
+++
@@ -1,4 +0,0 @@
-# once db is created and you've insterted the dummy data, run this
-# password prompt input `pass`
-#! /bin/bash
-psql -h localhost -p 5432 -d strends -U usr
--- init_db.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-docker run --name strends_db \
-    -e POSTGRES_PASSWORD=pass \
-    -e POSTGRES_USER=usr \
-    -e POSTGRES_DB=strends \
-    -p 5432:5432 \
-    -d postgres
