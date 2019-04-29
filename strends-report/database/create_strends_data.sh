d851928ee80ec022269be718f8087f8bab2d2635
Joe Saraceno <honestcomrade@gmail.com>
2019-04-29 10:30:18 -0600

    try to create a table manually after init

--- strends-report/database/create_strends_data.sh
+++ strends-report/database/create_strends_data.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
+echo "Initializing Tables..."
+
 docker create --name strends_data alpine \
     -v "/var/lib/postgresql/data"
+
+echo "Tables Initialized..."
--- strends-report/database/setup.sh
+++ strends-report/database/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-/etc/init.d/postgresql start
+# /etc/init.d/postgresql start
+# sleep 5
 psql -f create_fixtures.sql
-/etc/init.d/postgresql stop
+# /etc/init.d/postgresql stop
